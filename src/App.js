853c6f7785864ca313bbe31e35ff24104d2ecf00
Aivirth <aivirth.work@gmail.com>
2018-09-13 20:12:58 +0200

    Added prototype Navbar and Sidenav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import Navbar from "./components/Navigation/Navbar/Navbar";
+import Sidenav from "./components/Navigation/Sidenav/Sidenav";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <Navbar />
+        <Sidenav />
+      </div>
+    );
   }
 }
 
---
+++ src/components/Navigation/Navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default () => {
+  return (
+    <nav>
+      <div className="nav-wrapper container">
+        <a href="#!" className="brand-logo ">
+          Logo
+        </a>
+        <ul className="right hide-on-med-and-down">
+          <li>
+            <a href="sass.html">Sass</a>
+          </li>
+          <li>
+            <a href="badges.html">Components</a>
+          </li>
+          <li className="active">
+            <a href="collapsible.html">JavaScript</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
