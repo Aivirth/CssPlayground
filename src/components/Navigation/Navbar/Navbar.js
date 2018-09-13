30a51d8c66b3f4b6f797b4ba0b3969f0053171e1
Aivirth <aivirth.work@gmail.com>
2018-09-13 20:44:37 +0200

    Added basic menu items

--- src/components/Navigation/Navbar/Navbar.js
+++ src/components/Navigation/Navbar/Navbar.js
@@ -4,18 +4,15 @@ export default () => {
   return (
     <nav style={{ flex: "0 0 100%" }}>
       <div className="nav-wrapper container">
-        <a href="#!" className="brand-logo ">
-          Logo
+        <a href="/" className="brand-logo ">
+          CSS PlayGround
         </a>
         <ul className="right hide-on-med-and-down">
           <li>
-            <a href="sass.html">Sass</a>
-          </li>
-          <li>
-            <a href="badges.html">Components</a>
+            <a href="/">About</a>
           </li>
           <li className="active">
-            <a href="collapsible.html">JavaScript</a>
+            <a href="/">Github</a>
           </li>
         </ul>
       </div>
--- src/components/Navigation/Sidenav/Sidenav.js
+++ src/components/Navigation/Sidenav/Sidenav.js
@@ -9,7 +9,8 @@ export default () => {
         overflow: "auto",
         transform: "translateX(0px)",
         position: "static",
-        display: "inline-block"
+        display: "inline-block",
+        height: "calc(100vh - 64px)"
       }}
     >
       <li className="no-padding">
@@ -19,54 +20,18 @@ export default () => {
               className="collapsible-header waves-effect waves-teal"
               tabIndex="0"
             >
-              JavaScript
+              Lorem
             </a>
             <div className="collapsible-body" style={{ display: "block" }}>
               <ul>
-                <li>
-                  <a href="auto-init.html">Auto Init</a>
-                </li>
-                <li>
-                  <a href="carousel.html">Carousel</a>
-                </li>
-                <li>
-                  <a href="collapsible.html">Collapsible</a>
-                </li>
-                <li>
-                  <a href="dropdown.html">Dropdown</a>
-                </li>
-                <li>
-                  <a href="feature-discovery.html">FeatureDiscovery</a>
-                </li>
-                <li>
-                  <a href="media.html">Media</a>
-                </li>
-                <li>
-                  <a href="modals.html">Modals</a>
-                </li>
-                <li>
-                  <a href="parallax.html">Parallax</a>
-                </li>
-                <li>
-                  <a href="pushpin.html">Pushpin</a>
-                </li>
-                <li>
-                  <a href="scrollspy.html">Scrollspy</a>
-                </li>
                 <li className="active">
-                  <a href="sidenav.html">Sidenav</a>
+                  <a href="/">Border Radius</a>
                 </li>
                 <li>
-                  <a href="tabs.html">Tabs</a>
+                  <a href="/">Box Shadow</a>
                 </li>
                 <li>
-                  <a href="toasts.html">Toasts</a>
-                </li>
-                <li>
-                  <a href="tooltips.html">Tooltips</a>
-                </li>
-                <li>
-                  <a href="waves.html">Waves</a>
+                  <a href="/">Borders</a>
                 </li>
               </ul>
             </div>
@@ -75,13 +40,13 @@ export default () => {
       </li>
 
       <li className="bold">
-        <a href="showcase.html" className="waves-effect waves-teal">
-          Showcase
+        <a href="/" className="waves-effect waves-teal">
+          Lorem
         </a>
       </li>
       <li className="bold">
-        <a href="themes.html" className="waves-effect waves-teal">
-          Themes
+        <a href="/" className="waves-effect waves-teal">
+          Lorem
           <span className="new badge" />
         </a>
       </li>
