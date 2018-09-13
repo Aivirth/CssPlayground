84d5e56ebb62e03c435683fc050d8968f7b9cac7
Aivirth <aivirth.work@gmail.com>
2018-09-13 20:50:59 +0200

    Added Actor Component Prototype

---
+++ src/components/Actor/Actor.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+export default class Actor extends Component {
+  state = {
+    width: "300",
+    height: "300"
+  };
+
+  render() {
+    const styles = {
+      backgroundColor: "orangered",
+      border: "2px solid blue",
+      width: this.state.width + "px",
+      height: this.state.height + "px"
+    };
+
+    return <div id="Actor" style={styles} />;
+  }
+}
--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Actor from "../../components/Actor/Actor";
 
 export default () => {
   return (
     <main style={{ flex: "1", paddingLeft: "25px" }}>
       <h1>Lorem Ipsum</h1>
+      <Actor />
     </main>
   );
 };
