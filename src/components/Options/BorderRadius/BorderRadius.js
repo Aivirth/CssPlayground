726c2f1c2460bb5dca9d8115c06f12ec2a4f9a21
Aivirth <aivirth.work@gmail.com>
2018-09-16 19:25:10 +0200

    Added Rest of BorderRadius sliders

--- src/components/Options/BorderRadius/BorderRadius.js
+++ src/components/Options/BorderRadius/BorderRadius.js
@@ -16,11 +16,10 @@ class BorderRadius extends Component {
       ...this.props.borderRadius
     };
     console.log(updatedProperty);
-    const radiusDir= e.target.dataset.radiusdir;
-    console.log(radiusDir)
-    
+    const radiusDir = e.target.dataset.radiusdir;
+    console.log(radiusDir);
 
-    updatedProperty[e.target.dataset.identifier][radiusDir]= +e.target.value;
+    updatedProperty[e.target.dataset.identifier][radiusDir] = +e.target.value;
     this.props.updtBorderRadius(updatedProperty);
   };
 
@@ -43,6 +42,85 @@ class BorderRadius extends Component {
           changed={this.onRangeChange}
           identifier="borderTopLeftRadius"
           radiusDir="radiusX"
+        />
+        <Range
+          title="Border Top-Left (Y radius)"
+          min="0"
+          max="300"
+          value={borderTopLeftRadius.radiusY}
+          id="btl__y_radius"
+          changed={this.onRangeChange}
+          identifier="borderTopLeftRadius"
+          radiusDir="radiusY"
+        />
+
+        <hr style={{ margin: "20px 0" }} />
+
+        <Range
+          title="Border Top-Right (X radius)"
+          min="0"
+          max="300"
+          value={borderTopRightRadius.radiusX}
+          id="btr__x_radius"
+          changed={this.onRangeChange}
+          identifier="borderTopRightRadius"
+          radiusDir="radiusX"
+        />
+        <Range
+          title="Border Top-Right (Y radius)"
+          min="0"
+          max="300"
+          value={borderTopRightRadius.radiusY}
+          id="btr__y_radius"
+          changed={this.onRangeChange}
+          identifier="borderTopRightRadius"
+          radiusDir="radiusY"
+        />
+
+        <hr style={{ margin: "20px 0" }} />
+
+        <Range
+          title="Border Bottom-Right (X radius)"
+          min="0"
+          max="300"
+          value={borderBottomRightRadius.radiusX}
+          id="bbr__x_radius"
+          changed={this.onRangeChange}
+          identifier="borderBottomRightRadius"
+          radiusDir="radiusX"
+        />
+        <Range
+          title="Border Bottom-Right (Y radius)"
+          min="0"
+          max="300"
+          value={borderBottomRightRadius.radiusY}
+          id="bbr__y_radius"
+          changed={this.onRangeChange}
+          identifier="borderBottomRightRadius"
+          radiusDir="radiusY"
+        />
+
+        <hr style={{ margin: "20px 0" }} />
+
+        <Range
+          title="Border Bottom-Left (X radius)"
+          min="0"
+          max="300"
+          value={borderBottomLeftRadius.radiusX}
+          id="bbl__x_radius"
+          changed={this.onRangeChange}
+          identifier="borderBottomLeftRadius"
+          radiusDir="radiusX"
+        />
+        <Range
+          title="Border Bottom-Left (Y radius)"
+          min="0"
+          max="300"
+          value={borderBottomLeftRadius.radiusY}
+          id="bbl__y_radius"
+          changed={this.onRangeChange}
+          identifier="borderBottomLeftRadius"
+          radiusDir="radiusY"
         />
       </div>
     );
