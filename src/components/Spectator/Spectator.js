8fc0d71fee0923ff7a2869126c8ec7e8ea6704d6
Aivirth <aivirth.work@gmail.com>
2018-09-26 17:30:39 +0200

    Moved Spectator operations from componentDidUpdate to its own method

--- src/components/Spectator/Spectator.js
+++ src/components/Spectator/Spectator.js
@@ -3,6 +3,30 @@ import { connect } from "react-redux";
 import { getStyles } from "../../Redux/actions/actorActions";
 
 class Spectator extends PureComponent {
+  formatComputedStyleCssTextFromProps = () => {
+    const cssTextRaw = this.props.computedStyleCssText;
+
+    const cssText = cssTextRaw.split(";");
+    const cssTextCleaned = cssText.map(el => el.trim());
+    const cssTextAtomized = cssTextCleaned
+      .filter(el => el !== "")
+      .map(property => property.split(": "));
+
+    const cssTextProcessed = [];
+
+    cssTextAtomized.forEach(el => (cssTextProcessed[el[0]] = el[1]));
+
+    console.log(cssText);
+    console.log(cssTextCleaned);
+    console.log(cssTextAtomized);
+    console.log(cssTextProcessed);
+
+    const borderRadius = cssTextProcessed["border-radius"];
+    console.log(borderRadius);
+
+    //handling of each Border Radius case
+  };
+
   componentDidMount() {
     this.props.getStyles();
   }
@@ -10,27 +34,7 @@ class Spectator extends PureComponent {
   componentDidUpdate(prevProps) {
     if (this.props !== prevProps) {
       if (this.props.computedStyleCssText !== "") {
-        const cssTextRaw = this.props.computedStyleCssText;
-
-        const cssText = cssTextRaw.split(";");
-        const cssTextCleaned = cssText.map(el => el.trim());
-        const cssTextAtomized = cssTextCleaned
-          .filter(el => el !== "")
-          .map(property => property.split(": "));
-
-        const cssTextProcessed = [];
-
-        cssTextAtomized.forEach(el => (cssTextProcessed[el[0]] = el[1]));
-
-        console.log(cssText);
-        console.log(cssTextCleaned);
-        console.log(cssTextAtomized);
-        console.log(cssTextProcessed);
-
-        const borderRadius = cssTextProcessed["border-radius"];
-        console.log(borderRadius);
-
-        //handling of each Border Radius case
+        this.formatComputedStyleCssTextFromProps();
       }
     }
   }
