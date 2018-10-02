083deb1520520b3ada10d9c9b5b3db75a9b49d7a
Aivirth <aivirth.work@gmail.com>
2018-10-02 11:45:56 +0200

    Converted Sidenav to functional component

--- src/components/Navigation/Sidenav/Sidenav.js
+++ src/components/Navigation/Sidenav/Sidenav.js
@@ -1,37 +1,35 @@
 import React, { PureComponent } from "react";
 import { Link } from "react-router-dom";
 
-export default class Sidenav extends PureComponent {
-  render() {
-    return (
-      <React.Fragment>
-        <li className="sidenav-close">
-          <Link to="/">Base</Link>
-        </li>
-        <li className="sidenav-close">
-          <Link to="/border-radius">Border Radius</Link>
-        </li>
-        <li className="sidenav-close">
-          <Link to="/box-shadow">Box Shadow</Link>
-        </li>
-        <li className="sidenav-close">
-          <Link to="/borders">Borders</Link>
-        </li>
-        {/* <li className="active">
+export default () => {
+  return (
+    <React.Fragment>
+      <li className="sidenav-close">
+        <Link to="/">Base</Link>
+      </li>
+      <li className="sidenav-close">
+        <Link to="/border-radius">Border Radius</Link>
+      </li>
+      <li className="sidenav-close">
+        <Link to="/box-shadow">Box Shadow</Link>
+      </li>
+      <li className="sidenav-close">
+        <Link to="/borders">Borders</Link>
+      </li>
+      {/* <li className="active">
                   <Link to="/temp">lorem</Link>
                 </li> */}
-        <li className="bold">
-          <a href="/" className="waves-effect waves-teal">
-            Lorem
-          </a>
-        </li>
-        <li className="bold">
-          <a href="/" className="waves-effect waves-teal">
-            Lorem
-            <span className="new badge" />
-          </a>
-        </li>
-      </React.Fragment>
-    );
-  }
-}
+      <li className="bold">
+        <a href="/" className="waves-effect waves-teal">
+          Lorem
+        </a>
+      </li>
+      <li className="bold">
+        <a href="/" className="waves-effect waves-teal">
+          Lorem
+          <span className="new badge" />
+        </a>
+      </li>
+    </React.Fragment>
+  );
+};
