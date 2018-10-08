f2f4b170e6f83b511cfc0316c79ecf147db6d2da
Aivirth <aivirth.work@gmail.com>
2018-10-08 11:34:24 +0200

    Refactored Switch Component

--- src/components/UI/Input/Switch/Switch.js
+++ src/components/UI/Input/Switch/Switch.js
@@ -1,17 +1,17 @@
 import React from "react";
 
 export default props => {
-  const { changed, identifier, value } = props;
+  const { changed, identifier, value, htmlProperties } = props;
 
   return (
     <div className="switch">
       <label>
         Outline
         <input
-          type="checkbox"
           onChange={changed}
           data-identifier={identifier}
           value={value}
+          {...htmlProperties}
         />
         <span className="lever" />
         Inset
